644dceeae2e21773559950d016ce215f6ab95520
LucLor06 <LukeLorenzBA@gmail.com>
2024-11-21 21:53:56 -0500

    Create view and template for TodoApp. Add highlight.js.

--- todo_hitlist/main/urls.py
+++ todo_hitlist/main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('todo-apps/<slug>/', views.TodoAppDetailView.as_view(), name='todo-app'),
 ]
--- todo_hitlist/main/views.py
+++ todo_hitlist/main/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from .models import TodoApp
+from django.views.generic import DetailView
 
 
 def home(request):
     return render(request, 'home.html')
+
+
+class TodoAppDetailView(DetailView):
+    model = TodoApp
+    slug_url_kwarg = 'slug'
+    slug_field = 'slug'
+    template_name = 'todo_apps/todo-app.html'
+    context_object_name = 'todo_app'
